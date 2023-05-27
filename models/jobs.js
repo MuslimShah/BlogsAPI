5861df30ac78606cb47863177db54ad62179bde9
Muslim shah <smuslimshah7@gmail.com>
2023-05-27 18:49:48 +0500

    creating Posts model

---
+++ models/jobs.js
@@ -0,0 +1,35 @@
+const { Schema, default: mongoose } = require('mongoose');
+
+//creating user schema
+const jobsSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'provide the title']
+    },
+    content: {
+        type: String,
+        required: [true, 'provide the content']
+    },
+    comments: [{
+        userId: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+
+
+        },
+        comment: {
+            type: String,
+
+        }
+
+    }],
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model("User", UserSchema);
