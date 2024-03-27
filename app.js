df34bb02698590505203135bf252ec34533808e4
muslimshah <smuslimshah7@gmail.com>
2024-03-27 20:48:19 +0500

    changing configuration

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
  * Project name: Blogs API
  * Author: Muslim Shah
  * Date: 28/5/2023
- * 
+ *
  * Description: Blogs API in which users can create an account, login, create, update, and delete blogs,
  * read their own and other users' blogs, comment on a blog, and delete their comments.
- * 
+ *
  * Routes Handled:
  * - api/v1/auth/register
  * - api/v1/auth/login (GET)
@@ -20,60 +20,66 @@
  *    - Post Comment
  *       - PATCH api/v1/users/blogs/:id
  *       - Delete Comment (DELETE api/v1/users/blogs/comments/:commentId)
- * 
+ *
  * Security Packages Used:
  * - helmet: Provides various security-related HTTP headers.
  * - cors: Enables Cross-Origin Resource Sharing (CORS) for handling requests from different domains.
  * - xss-clean: Prevents Cross-Site Scripting (XSS) attacks by sanitizing user input.
  * - express-limiter: Implements rate limiting to protect against brute force and denial-of-service attacks.
- * 
+ *
  * Error Handling:
  * - express-async-errors: Handles asynchronous errors in Express.js.
  *********************************************/
 
-
 //importing required files from import.js
 const {
-    express,
-    authRouts,
-    blogsRoutes,
-    connectDB,
-    pageNotFound,
-    errorHandler,
-    cors,
-    helmet,
-    xss,
-    rateLimit
-} = require('./config/import');
-const auth = require('./utils/auth')
+  express,
+  authRouts,
+  blogsRoutes,
+  connectDB,
+  pageNotFound,
+  errorHandler,
+  cors,
+  helmet,
+  xss,
+  rateLimit,
+} = require("./config/import");
+require("dotenv").config();
+const auth = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(rateLimit({
+app.use(
+  rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-}))
-app.set('view engine', 'views');
+  })
+);
+app.set("view engine", "views");
 
 //routes
-app.use('/api/v1/auth', authRouts);
-app.use('/api/v1/', auth, blogsRoutes);
+app.use("/api/v1/auth", authRouts);
+app.use("/api/v1/", auth, blogsRoutes);
 
 //page not found middleware
 app.use(pageNotFound);
 //error handler middleware
 app.use(errorHandler);
 //start function for starting server
-const start = async() => {
-    //connecting to db
-    console.log('Initializing ...');
-    connectDB(process.env.MONGODB_URI);
-    app.listen(PORT, () => console.log(`CONNECTED ON PORT ${PORT}`))
-}
+const start = async () => {
+  //connecting to db
+  console.log("Initializing ...");
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(PORT, () => console.log(`CONNECTED ON PORT ${PORT}`));
+  } catch (error) {
+    console.log("database connection error");
+  }
+};
 start();
--- models/user.js
+++ models/user.js
@@ -1,67 +1,70 @@
-const { Schema, default: mongoose } = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { Schema, default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 //creating user schema
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'please provide name'],
-        minlength: 3,
-        maxlength: 40,
-        match: [/^[a-zA-Z\s]+$/, 'name must be alphabets only']
+      type: String,
+      required: [true, "please provide name"],
+      minlength: 3,
+      maxlength: 40,
+      match: [/^[a-zA-Z\s]+$/, "name must be alphabets only"],
     },
     email: {
-        type: String,
-        required: [true, 'you must provide email'],
-        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'must provide a valid email'],
-        unique: [true, 'must provide a unique email']
+      type: String,
+      required: [true, "you must provide email"],
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "must provide a valid email"],
+      unique: [true, "must provide a unique email"],
     },
     password: {
-        type: String,
-        required: [true, 'must provide the password'],
-        minlength: [, 'password must be atleast 8 characters'],
-        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, 'must provide correct password']
-
+      type: String,
+      required: [true, "must provide the password"],
+      minlength: [, "password must be atleast 8 characters"],
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+        "must provide correct password",
+      ],
     },
     gender: {
-        type: String,
-        required: [true, 'must specify the gender'],
-        enum: ['Male', "Female", "Other"],
-        default: "Male"
+      type: String,
+      required: [true, "must specify the gender"],
+      enum: ["Male", "Female", "Other"],
+      default: "Male",
     },
     dateOfBirth: {
-        type: Date,
-        required: [true, 'must provide Date Of Birth']
+      type: Date,
+      required: [true, "must provide Date Of Birth"],
     },
     phoneNumber: {
-        type: String
+      type: String,
     },
     country: {
-        type: String
-    }
-
-}, { timestamps: true });
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 //hasing password before saving into db
-UserSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 //comparing password...
-UserSchema.methods.comparePassword = async function(candidatePassword) {
-    const isMatched = bcrypt.compare(candidatePassword, this.password);
-    return isMatched;
-}
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatched = bcrypt.compare(candidatePassword, this.password);
+  return isMatched;
+};
 
 //creating token
-UserSchema.methods.createToken = async function() {
-    const secretKey = process.env.JWT_SECRET;
-    const payLoad = { userId: this._id, name: this.name };
-    return jwt.sign(payLoad, secretKey, { expiresIn: process.env.JWT_LIFETIME });
-}
-
-
+UserSchema.methods.createToken = async function () {
+  const secretKey = process.env.JWT_SECRET;
+  console.log(secretKey);
+  const payLoad = { userId: this._id, name: this.name };
+  return jwt.sign(payLoad, secretKey, { expiresIn: process.env.JWT_LIFETIME });
+};
 
 module.exports = mongoose.model("User", UserSchema);
