fc8303d2e6acfd36107664a3a2b44e03e07fbb22
muslimshah <smuslimshah7@gmail.com>
2025-01-15 22:50:34 +0500

    allowing cors from all origins

--- app.js
+++ app.js
@@ -52,7 +52,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(xss());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
@@ -67,7 +73,7 @@ app.set("view engine", "views");
 
 //routes
 app.use("/api/v1/auth", authRouts);
-app.use("/api/v1/", auth,blogsRoutes);
+app.use("/api/v1/", auth, blogsRoutes);
 
 //page not found middleware
 app.use(pageNotFound);
